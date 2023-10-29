22fb40d2abb89609024309a1a9b9792adbd6440c
zointarasbangun <zointa.120140225@student.itera.ac.id>
2023-10-30 02:16:21 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,27 @@ Route::get('/tambahakun', function () {
 Route::get('/dataakun', function () {
     return view('dataakun');
 });
+
+Route::get('/dataperjalanan', function () {
+    return view('dataperjalanan');
+});
+
+Route::get('/statusperjalanan', function () {
+    return view('statusperjalanan');
+});
+
+Route::get('/pelacakperjalanan', function () {
+    return view('pelacakperjalanan');
+});
+
+Route::get('/datakondisikendaraan', function () {
+    return view('datakondisikendaraan');
+});
+
+Route::get('/tipekendaraan', function () {
+    return view('tipekendaraan');
+});
+
+Route::get('/notifikasi', function () {
+    return view('notifikasi');
+});
