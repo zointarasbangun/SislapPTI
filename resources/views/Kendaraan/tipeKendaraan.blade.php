03ae7497d2d19c32c9450b9115a4184f89c4e762
jacksih <jackysihombing33@gmail.com>
2023-11-08 08:37:09 +0700

    bekend kendaraan

--- resources/views/Kendaraan/tipeKendaraan.blade.php
+++ resources/views/Kendaraan/tipeKendaraan.blade.php
@@ -14,11 +14,62 @@
             </div>
             <div class="col-lg-3 col-sm-12">
                 <div class="float-right">
+                    <!-- Modal -->
+                    <div class="modal fade" id="modalCarForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header text-start">
+                                    <h4 class="modal-title w-100 font-weight-bold">Tambah Data Kendaraan</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <form action="{{ route('kendaraan.store') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="modal-body mx-3">
+                                        <div class="mb-5">
+                                            <i class="iconify nav-icon mr-3" data-icon="mdi:car-select"></i>
+                                            <label data-error="wrong" data-success="right" for="defaultForm-tipe">Tipe Mobil :</label>
+                                            <input type="text" id="defaultForm-tipe" name="type" class="form-control validate" placeholder="contoh : Toyota || Avanza">
+                                        </div>
 
+                                        <div class="mb-5">
+                                            <i class="iconify nav-icon mr-3" data-icon="iconoir:input-field"></i>
+                                            <label data-error="wrong" data-success="right" for="defaultForm-nopol">No. Polisi :</label>
+                                            <input type="text" id="defaultForm-nopol" name="no_polisi" class="form-control validate" placeholder="BE-XXXX-XX">
+                                        </div>
+
+                                        <div class="mb-5">
+                                            <i class="iconify nav-icon mr-3" data-icon="streamline:interface-calendar-download-arrow-calendar-date-day-down-download-month"></i>
+                                            <label data-error="wrong" data-success="right" for="defaultForm-tgl">Tanggal Masuk :</label>
+                                            <input type="date" id="defaultForm-tgl" name="tgl_masuk" class="form-control validate">
+                                        </div>
+
+                                        <div class="mb-4">
+                                            <i class="iconify nav-icon mr-3" data-icon="tabler:photo-plus"></i>
+                                            <label data-error="wrong" data-success="right" for="defaultForm-foto">Foto Kendaraan :</label>
+                                            <input type="file" id="defaultForm-foto" name="photo" class="form-control-file">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer d-flex justify-content-center">
+                                        <button type="submit" class="btn btn-success">Tambah Data</button>
+                                        <button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close">Batal</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /modal -->
+
+                    <!-- Button Modal -->
+                    <div class="text-center">
+                        <a href="" class="btn btn-primary btn-rounded mb-4" data-toggle="modal" data-target="#modalCarForm"><i class="iconify nav-icon mr-3" data-icon="gridicons:add"></i>Tambah Data Kendaraan</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
     <!-- /header content -->
     <div class="container-fluid">
         <div class="row mt-2">
@@ -35,6 +86,7 @@
                 <th scope="col">No. Polisi</th>
                 <th scope="col">Tanggal Masuk</th>
                 <th scope="col">Foto Kendaraan</th>
+                <th scope="col">Aksi</th>
             </tr>
             @foreach ($kendaraans as $kendaraan)
             <tr>
@@ -42,8 +94,9 @@
                 <td>{{ $kendaraan->no_polisi }}</td>
                 <td>{{ $kendaraan->tgl_masuk }}</td>
                 <td>
+                    @if ($kendaraan->photo)
                     <!-- Modal -->
-                    <div class="modal fade" id="modalLihat" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="modalLihat{{ $kendaraan->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header text-start">
@@ -54,7 +107,7 @@
                                 </div>
                                 <div class="modal-body mx-3">
                                     <div class="mb-0">
-                                    <img src="{{ URL('lte/dist/img/nissanGTR.webp') }}" class="img-fluid"/>
+                                        <img src="{{ asset('storage/' . $kendaraan->photo) }}" class="img-fluid" alt="Foto Kendaraan"/>
                                     </div>
                                 </div>
                                 <div class="modal-footer d-flex justify-content-right">
@@ -67,20 +120,26 @@
 
                     <!-- Button Modal -->
                     <div class="text-center">
-                        <a href="" class="btn btn-primary btn-rounded mb-4" data-toggle="modal" data-target="#modalLihat"><i class="iconify nav-icon mr-3" data-icon="mdi:eye"></i>Lihat Foto</a>
+                        <a href="#" class="btn btn-primary btn-rounded mb-4" data-toggle="modal" data-target="#modalLihat{{ $kendaraan->id }}"><i class="iconify nav-icon mr-3" data-icon="mdi:eye"></i>Lihat Foto</a>
                     </div>
                     <!-- /button modal -->
+                    @else
+                    Gambar tidak tersedia
+                    @endif
                 </td>
                 <td>
-                    <button type="button" class="btn btn-primary"><i class="iconify" data-icon="material-symbols:edit"></i></button>
-                    <button type="button" class="btn btn-danger"><i class="iconify" data-icon="material-symbols:delete"></i></button>
+                    <a href="{{ route('kendaraan.edit', $kendaraan->id) }}" class="btn btn-primary"><i class="iconify" data-icon="material-symbols:edit"></i></a>
+                    <form action="{{ route('kendaraan.destroy', $kendaraan->id) }}" method="POST" style="display: inline">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger"><i class="iconify" data-icon="material-symbols:delete"></i></button>
+                    </form>
                 </td>
             </tr>
             @endforeach
-
-
         </table>
     </div>
+
     <!-- /table -->
 </div>
 
