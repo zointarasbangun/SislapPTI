c516e3bc02de0736623d8bf1f007bf11a0813d69
zointarasbangun <zointa.120140225@student.itera.ac.id>
2023-12-18 21:06:23 +0700

    Revert "fix migrate"
    This reverts commit 3e8ad80bd1aec08782439a41450222e9f1c94074.

--- database/migrations/2023_11_08_024255_perjalanans.php
+++ database/migrations/2023_11_08_024255_perjalanans.php
@@ -16,9 +16,7 @@ return new class extends Migration
     {
         Schema::create('perjalanans', function (Blueprint $table) {
             $table->id();
-            // $table->foreignId('user_id')->constrained();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained();
             $table->date('tgl_perjalanan');
             $table->string('alamat_awal');
             $table->string('alamat_tujuan');
@@ -48,7 +46,7 @@ return new class extends Migration
             $table->string('catatan')->nullable();
             $table->string('status_perjalanan')->default(Perjalanan::STATUS_MENUNGGU);
             $table->timestamps();
-
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
     }
--- database/migrations/2023_11_17_180943_profiles.php
+++ database/migrations/2023_11_17_180943_profiles.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('fulname')->nullable();
             $table->string('username')->unique()->nullable();
             $table->string('phone')->nullable();
@@ -23,6 +21,7 @@ return new class extends Migration
             $table->text('bio')->nullable();
             $table->string('foto_profil')->nullable();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
